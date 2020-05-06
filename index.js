52fc576d62a2506939a309a15c2ac90e6e1cc881
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-05-07 00:36:29 +0500

    Configure handlebars

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express"); 
 const path = require("path");
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => { // to handle GET request
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "vies");
+
+app.get("/", (req, res) => { 
   res.status(200);
   res.sendFile(path.join(__dirname, "views", "index.html"));
 }); 
