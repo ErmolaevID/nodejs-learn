81090cdb047e6bf28b7e6c72d5d9e9c71f46141b
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-05-06 22:17:26 +0500

    Server can response html

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express"); 
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => { // to handle GET request
+  res.status(200);
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+}); 
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
