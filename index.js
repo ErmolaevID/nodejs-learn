1f833af5741e0c8ed1c73a5fce6c869354268c62
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-05-06 22:45:24 +0500

    Add about.html

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => { // to handle GET request
   res.status(200);
   res.sendFile(path.join(__dirname, "views", "index.html"));
 }); 
+app.get("/about.html", (req, res) => {
+  res.status(200);
+  res.sendFile(path.join(__dirname, "views", "about.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
