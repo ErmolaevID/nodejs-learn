101556194572839497fed074dfc789f8f3bd3d28
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-05-08 17:21:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.set("view engine", "hbs");
 app.set("views", "views");
 
 app.use(express.static("public"));
-app.use(homeRoutes);
-app.use(coursesRoutes);
-app.use(addRoutes);
+app.use("/", homeRoutes);
+app.use("/courses", coursesRoutes);
+app.use("/add", addRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
